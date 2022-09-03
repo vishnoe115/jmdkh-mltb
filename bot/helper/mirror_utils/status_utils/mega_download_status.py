91b0d703d960da62cdc48f057fc949a18c003b5b
Juned KH <kjuned007@gmail.com>
2022-09-03 16:25:10 +0530

    Minor Change Add:All common cmd are here Improve:each status

--- bot/helper/mirror_utils/status_utils/mega_download_status.py
+++ bot/helper/mirror_utils/status_utils/mega_download_status.py
@@ -6,7 +6,6 @@ class MegaDownloadStatus:
         self.__obj = obj
         self.__uid = listener.uid
         self.message = listener.message
-        self.__engine = "megasdkrestclient"
         self.__mode = listener.mode
 
     def gid(self):
@@ -53,7 +52,7 @@ class MegaDownloadStatus:
         return self.__obj
 
     def engine(self):
-        return self.__engine
+        return "megasdkrestclient"
 
     def source(self):
         reply_to = self.message.reply_to_message
